e2ea2069e422fbc23da9b02cf9096542c5860042
Matthew Mitchell <mmitchell@P6HQ4G514K-MITCHELL.local>
2024-01-11 12:30:30 -0600

    Added docstring

--- src/format.rs
+++ src/format.rs
@@ -1,11 +1,15 @@
 use clap_num::number_range;
 use unicode_segmentation::UnicodeSegmentation;
 
-pub fn shorten(string: String, length: usize, trail: &str) -> String {
+/// Shorten a unicode string and add a trail at the end (ex. ...).
+/// Can handle Chinese script, emojis, and other scripts.
+/// Args:
+///     string - the string to shorten
+///     len - The length to shorten the description to
+///     trail - What to add at the end of the description (ex. ...)
+pub fn shorten_unicode(string: String, length: usize, trail: &str) -> String {
     let graphemes = string.grapheme_indices(true);
-    // let trimmed = &string[..length];
     let graph_vec: Vec<_> = graphemes.take(length).into_iter().collect();
-    // let trimmed = graph_vec[..length].join("-");
     let mut unpacked: Vec<&str> = vec![];
     for grapheme in graph_vec {
         unpacked.push(grapheme.1)
